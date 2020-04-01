fc5392433a1855eb7493402dc83782881132a542
KITSViktorSkibnev <48727181+KITSViktorSkibnev@users.noreply.github.com>
2020-04-01 23:52:25 +0300

    EDGOAIPMH-38 Use "Accept: text/xml" or empty Accept when making requests to mod-oai-pmh (#33)
    * EDGOAIPMH-38 Use "Accept: text/xml" or empty Accept when making requests to mod-oai-pmh

--- src/test/java/org/folio/edge/oaipmh/MainVerticleTest.java
+++ src/test/java/org/folio/edge/oaipmh/MainVerticleTest.java
@@ -536,7 +536,7 @@ public class MainVerticleTest {
 
     final Response resp = RestAssured
       .given()
-      .header(HttpHeaders.ACCEPT.toString(), "bogus")
+      .header(HttpHeaders.ACCEPT, TEXT_XML)
       .get(String.format("/oai?verb=GetRecord"
         + "&identifier=oai:arXiv.org:cs/0112017&metadataPrefix=oai_dc&apikey=%s", API_KEY))
       .then()
@@ -559,6 +559,79 @@ public class MainVerticleTest {
     String expectedMockBody = OaiPmhMockOkapi.getOaiPmhResponseAsXml(expectedMockPath);
 
     final Response resp = RestAssured
+      .get(String.format("/oai?verb=GetRecord"
+        + "&identifier=oai:arXiv.org:cs/0112017&metadataPrefix=oai_dc&apikey=%s", API_KEY))
+      .then()
+      .log().all()
+      .contentType(Constants.TEXT_XML_TYPE)
+      .statusCode(HttpStatus.SC_OK)
+      .header(HttpHeaders.CONTENT_TYPE, Constants.TEXT_XML_TYPE)
+      .extract()
+      .response();
+
+    String actualBody = resp.body().asString();
+    assertEquals(expectedMockBody, actualBody);
+  }
+
+  @Test
+  public void testAcceptHeaderHasUnsupportedType() {
+    logger.info("=== Test handling of unsupported type in Accept header ===");
+
+    String unsupportedAcceptType = "application/json";
+
+    final Response resp = RestAssured
+      .given()
+      .header(HttpHeaders.ACCEPT, unsupportedAcceptType)
+      .get(String.format("/oai?verb=GetRecord"
+        + "&identifier=oai:arXiv.org:cs/0112017&metadataPrefix=oai_dc&apikey=%s", API_KEY))
+      .then()
+      .log().all()
+      .contentType(Constants.TEXT_XML_TYPE)
+      .statusCode(HttpStatus.SC_NOT_ACCEPTABLE)
+      .header(HttpHeaders.CONTENT_TYPE, Constants.TEXT_XML_TYPE)
+      .extract()
+      .response();
+
+    String actualBody = resp.body().asString();
+    String expectedBody = "Accept header must be \"text/xml\" for this request, but it is " +"\""+ unsupportedAcceptType
+      +"\""+", can not send */*";
+    assertEquals(expectedBody, actualBody);
+  }
+
+  @Test
+  public void testAcceptHeaderIsAbsent() {
+    logger.info("=== Test Accept header is absent ===");
+
+    Path expectedMockPath = Paths.get(OaiPmhMockOkapi.PATH_TO_GET_RECORDS_MOCK);
+    String expectedMockBody = OaiPmhMockOkapi.getOaiPmhResponseAsXml(expectedMockPath);
+
+    final Response resp = RestAssured
+      .given()
+      .header(HttpHeaders.ACCEPT_CHARSET, "utf-8")
+      .get(String.format("/oai?verb=GetRecord"
+        + "&identifier=oai:arXiv.org:cs/0112017&metadataPrefix=oai_dc&apikey=%s", API_KEY))
+      .then()
+      .log().all()
+      .contentType(Constants.TEXT_XML_TYPE)
+      .statusCode(HttpStatus.SC_OK)
+      .header(HttpHeaders.CONTENT_TYPE, Constants.TEXT_XML_TYPE)
+      .extract()
+      .response();
+
+    String actualBody = resp.body().asString();
+    assertEquals(expectedMockBody, actualBody);
+  }
+
+  @Test
+  public void testAcceptHeaderIsEmpty() {
+    logger.info("=== Test Accept header is empty ===");
+
+    Path expectedMockPath = Paths.get(OaiPmhMockOkapi.PATH_TO_GET_RECORDS_MOCK);
+    String expectedMockBody = OaiPmhMockOkapi.getOaiPmhResponseAsXml(expectedMockPath);
+
+    final Response resp = RestAssured
+      .given()
+      .header(HttpHeaders.ACCEPT, "*/*")
       .get(String.format("/oai?verb=GetRecord"
         + "&identifier=oai:arXiv.org:cs/0112017&metadataPrefix=oai_dc&apikey=%s", API_KEY))
       .then()
